92a8fa94f95663d837adc53cd8f723122f2d6c75
anastazjasta <aneta.dakalo@gmail.com>
2020-04-29 19:34:51 +0200

    feat: fetch repos list

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,14 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
+const list = document.querySelector('.list--js');
+fetch('https://api.github.com/users/anastazjasta/repos')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+  for (const repo of repos) {
+    list.innerHTML += `<li><a href="${repo.html_url}">${repo.name}</a></li>`;
+  }
+  }) 
+.catch (err => {
+  console.log(err);
+})
