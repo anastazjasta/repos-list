b1bfbd4003030c2c7229321756d0a734c9788312
Anastazjasta <anastazja.sta@gmail.com>
2020-05-13 15:06:59 +0200

    feat: any github user

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,30 @@
 "use strict";
 
-const list = document.querySelector('.list--js');
-fetch('https://api.github.com/users/anastazjasta/repos')
-  .then(resp => resp.json())
-  .then(resp => {
-    const repos = resp;
-  for (const repo of repos) {
-    const {html_url, name} = repo;
-    list.innerHTML += 
-    `<li class="list__item">
-      <a class="list__link" href="${html_url}">${name}</a>
-    </li>`;
-  }
-  }) 
-.catch (err => {
-  console.log(err);
-})
+
+const button = document.querySelector('.submit--js');
+
+
+button.addEventListener('click', (e) => {
+  let input = document.querySelector('.username--js');
+  let username = input.value;
+  let title = document.querySelector('.title--js');
+  const list = document.querySelector('.list--js');
+  list.innerHTML = ``;
+  title.innerHTML = `List of public repositories of GitHub user ${username}`;
+
+  fetch(`https://api.github.com/users/${username}/repos`)
+    .then(resp => resp.json())
+    .then(resp => {
+      const repos = resp;
+    for (const repo of repos) {
+      const {html_url, name} = repo;
+      list.innerHTML += 
+      `<li class="list__item">
+        <a class="list__link" href="${html_url}">${name}</a>
+      </li>`;
+    }
+    }) 
+  .catch (err => {
+    console.log(err);
+  })
+} );
