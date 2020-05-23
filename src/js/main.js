2c7148fef74aae417f76d082b50ea1c747ba59b7
anastazjasta <aneta.dakalo@gmail.com>
2020-05-23 20:06:08 +0200

    feat: open graph

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,28 @@
 "use strict";
 
+const button = document.querySelector(".submit--js");
 
-const button = document.querySelector('.submit--js');
-
-
-button.addEventListener('click', (e) => {
-  let input = document.querySelector('.username--js');
+button.addEventListener("click", (e) => {
+  let input = document.querySelector(".username--js");
   let username = input.value;
-  let title = document.querySelector('.title--js');
-  const list = document.querySelector('.list--js');
+  let title = document.querySelector(".title--js");
+  const list = document.querySelector(".list--js");
   list.innerHTML = ``;
   title.innerHTML = `List of public repositories of GitHub user ${username}`;
 
   fetch(`https://api.github.com/users/${username}/repos`)
-    .then(resp => resp.json())
-    .then(resp => {
+    .then((resp) => resp.json())
+    .then((resp) => {
       const repos = resp;
-    for (const repo of repos) {
-      const {description, html_url, name} = repo;
-      list.innerHTML += 
-      `<li class="list__item">
+      for (const repo of repos) {
+        const { description, html_url, name } = repo;
+        list.innerHTML += `<li class="list__item">
         <a class="list__link" href="${html_url}">${name}</a>
         <p class="list__description">${description ? description : ""}</p>
       </li>`;
-    }
-    }) 
-  .catch (err => {
-    console.log(err);
-  })
-} );
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
